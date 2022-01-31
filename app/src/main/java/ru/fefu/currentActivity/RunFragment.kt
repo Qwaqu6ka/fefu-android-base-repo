452c65440bc4c60b00be69fb8ea632a981b3f338
Qwaqu6ka <r.bortsov@yandex.ru>
2022-01-31 15:23:04 +1000

    add run fragment info

--- app/src/main/java/ru/fefu/currentActivity/RunFragment.kt
+++ app/src/main/java/ru/fefu/currentActivity/RunFragment.kt
@@ -1,11 +1,13 @@
 package ru.fefu.currentActivity
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.TextView
 import org.osmdroid.util.GeoPoint
+import ru.fefu.activitytracker.CoordToDistance
 import ru.fefu.activitytracker.LocationService
 import ru.fefu.activitytracker.R
 import ru.fefu.activitytracker.databinding.FragmentRunBinding
@@ -16,8 +18,11 @@ import ru.fefu.database.Point
 class RunFragment : BaseFragment<FragmentRunBinding>(R.layout.fragment_run) {
 
 
+    @SuppressLint("SetTextI18n")
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.timer.text = "00:01:46"
 
         val activity = (requireActivity() as MapActivity)
 
@@ -32,10 +37,24 @@ class RunFragment : BaseFragment<FragmentRunBinding>(R.layout.fragment_run) {
                 MapActivity.lastDrawnPoint =
                     Point(it.coordinates.last().latitude, it.coordinates.last().longitude)
             }
+
+            val distance = CoordToDistance.getDistanceFromLatLonInM(it.coordinates)
+            if (distance > 1000)
+                binding.distance.text = "${"%.2f".format(distance / 1000)} км"
+            else
+                binding.distance.text = "${distance.toInt()} м"
         }
 
 
         val active = App.INSTANCE.db.activeDao().getLastActive()
+        binding.activeText.text = active.activeType.value
+
+        val distance = CoordToDistance.getDistanceFromLatLonInM(active.coordinates)
+        if (distance > 1000)
+            binding.distance.text = "${"%.2f".format(distance / 1000)} км"
+        else
+            binding.distance.text = "${distance.toInt()} м"
+
         for (point in active.coordinates) {
                 val p = GeoPoint(
                     point.latitude,
